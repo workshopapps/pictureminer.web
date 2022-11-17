50e603d5c8d37586e8f277853bff323f3f2abb56
Mayowa Sodipo <sodipomayowa7@gmail.com>
2022-11-17 16:33:48 +0100

    Clean the repo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import RoutesComponents from "./routes";
-import UserProvider from "./context/UserProvider";
+
 import "./index.css";
 
 const IndexPage = () => {
@@ -19,9 +19,9 @@ const IndexPage = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <UserProvider>
-    <IndexPage />
-  </UserProvider>
+
+  <IndexPage />
+
   // </React.StrictMode>
 );
 
