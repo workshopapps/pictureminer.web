c05382dc35b72f3ae0bf8fdc0fe06e30cc1dc81a
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-11-18 19:39:30 +0100

    feat: Added career application page

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,15 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import ErrorPage from './Error';
 import App from './App';
+import { CareerApplication } from './pages/career';
 
 const RoutesComponents = () => {
   return (
     <Layout>
       <Routes>
-        <Route path="" element={<App />}>
-          <Route index path="/" element={<App />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
-        </Route>
+        {/* <Route path="" element={<App />}> */}
+        <Route index path="/" element={<App />}></Route>
+
+        <Route path="/careers/application" element={<CareerApplication />} />
+        <Route path="*" element={<ErrorPage />}></Route>
+        {/* </Route> */}
       </Routes>
     </Layout>
   );
