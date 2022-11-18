d9d3faab0e29360d0919672c5f3bdfaa4571dd60
Mayowa Sodipo <sodipomayowa7@gmail.com>
2022-11-18 19:49:11 +0100

    feat(Fe-21): Created the dashboard sidebar and navbar

--- src/layouts/dashboard/DashboardLayout.jsx
+++ src/layouts/dashboard/DashboardLayout.jsx
@@ -3,14 +3,14 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 const DashboardLayout = () => {
-  return (
-    <div className="dashboard_layout">
-      <Sidebar />
-      <div className="dashboard_pages">
-        <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div className="dashboard_layout">
+			<Sidebar />
+			<div className="dashboard_pages">
+				<Outlet />
+			</div>
+		</div>
+	);
 };
 
 export default DashboardLayout;
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,18 +4,23 @@ import { Routes, Route } from 'react-router-dom';
 // import Layout from "./layouts/Layout";
 import ErrorPage from './Error';
 import App from './App';
+import DashboardLayout from './layouts/dashboard/DashboardLayout';
+import Dashboard from './pages/dashboard/Dashboard';
 
 const RoutesComponents = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="" element={<App />}>
-          <Route index path="/" element={<App />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="" element={<App />}>
+					<Route index path="/" element={<App />}></Route>
+					<Route path="*" element={<ErrorPage />}></Route>
+				</Route>
+				<Route path="" element={<DashboardLayout />}>
+					<Route path="/dashboard" element={<Dashboard />} />
+				</Route>
+			</Routes>
+		</>
+	);
 };
 
 export default RoutesComponents;
