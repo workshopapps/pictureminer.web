538875e2aed40649154e3929ab9802903a6be5f8
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-11-18 11:43:12 +0100

    fix: merge from dev

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React from 'react';
 // import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
