d3f5229fbebf9f1e71cdd75e91d9084a8ca81465
Shadrach Akaade <akaadesm@gmail.com>
2022-11-19 15:11:01 +0100

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,19 +4,45 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import ErrorPage from './Error';
 import App from './App';
+
 import Termsofuse from './pages/terms-of-use/Termsofuse';
+
+import { CareerApplication } from './pages/career';
+
+import DashboardLayout from './layouts/dashboard/DashboardLayout';
+import Dashboard from './pages/dashboard/Dashboard';
+import DocumentationHome from './pages/documentation/Home/DocumentationHome';
+import GettingStarted from './pages/documentation/GettingStartedPage/GettingStarted';
+import Installations from './pages/documentation/InstallationsPage/Installations';
 
 const RoutesComponents = () => {
   return (
-    <Layout>
+    <>
       <Routes>
-        <Route>
+        <Route path="" element={<Layout />}>
           <Route index path="/" element={<App />}></Route>
+          <Route
+            index
+            path="/documentation"
+            element={<DocumentationHome />}
+          ></Route>
+          <Route
+            path="/documentation/getting-started"
+            element={<GettingStarted />}
+          ></Route>
+          <Route
+            path="/documentation/installations"
+            element={<Installations />}
+          ></Route>
+          <Route path="/careers/application" element={<CareerApplication />} />
           <Route path="*" element={<ErrorPage />}></Route>
           <Route path="/terms-of-use" element={<Termsofuse />}></Route>
         </Route>
+        <Route path="" element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
-    </Layout>
+    </>
   );
 };
 
