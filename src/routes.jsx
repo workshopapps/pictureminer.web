90650de8e853e35af648c1d6f81dbcb8a780658e
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-19 10:32:11 +0100

    chore: setup

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,23 +10,20 @@ import DashboardLayout from './layouts/dashboard/DashboardLayout';
 import Dashboard from './pages/dashboard/Dashboard';
 
 const RoutesComponents = () => {
-
-	return (
-		<>
-			<Routes>
-				<Route path="" element={<Layout />}>
-					<Route index path="/" element={<App />}></Route>
-           <Route path="/careers/application" element={<CareerApplication />} />
-					<Route path="*" element={<ErrorPage />}></Route>
-				</Route>
-				<Route path="" element={<DashboardLayout />}>
-					<Route path="/dashboard" element={<Dashboard />} />
-				</Route>
-			</Routes>
-		</>
-	);
-
-
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route index path="/" element={<App />}></Route>
+          <Route path="/careers/application" element={<CareerApplication />} />
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Route>
+        <Route path="" element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default RoutesComponents;
