13d996921f810ed87eee2724e16a36da529f47f1
taofeek-ob <taofeek01@yahoo.com>
2022-11-19 11:42:24 +0100

    refactor (FE-30): added proptypes to components

--- src/features/documentation/components/DocumentationSidebar.js
+++ src/features/documentation/components/DocumentationSidebar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ScrollspyNav from 'react-scrollspy-nav';
+import PropTypes from 'prop-types';
 import './DocumentationSidebar.scss';
-const DocumentationSidebar = ({ sectionIds, linkTexts }) => {
+const DocumentationSidebar = (props) => {
   return (
     <div className="documentation-sidebar">
       <h3 className="documentation-sidebar_title">On this page</h3>
       <div>
         <ScrollspyNav
-          scrollTargetIds={sectionIds}
+          scrollTargetIds={props.sectionIds}
           activeNavClass="documentation-sidebar_active"
           scrollDuration="500"
         >
-          {linkTexts.map((link, index) => (
-            <a href={`#${sectionIds[index]}`} key={index} className="documentation-sidebar_section">
+          {props.linkTexts.map((link, index) => (
+            <a
+              href={`#${props.sectionIds[index]}`}
+              key={index}
+              className="documentation-sidebar_section"
+            >
               {link}
             </a>
           ))}
@@ -20,6 +25,11 @@ const DocumentationSidebar = ({ sectionIds, linkTexts }) => {
       </div>
     </div>
   );
+};
+
+DocumentationSidebar.propTypes = {
+  sectionIds: PropTypes.array,
+  linkTexts: PropTypes.array,
 };
 
 export default DocumentationSidebar;
