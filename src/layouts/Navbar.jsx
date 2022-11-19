024ee0e35e210db0d2c3a3e8d637b218cbcc03c9
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-11-19 14:38:56 +0100

    fix: Wrapped navbar logo in a link to navigate to home when clicked

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -8,13 +8,15 @@ const NavBar = () => {
 
   return (
     <nav className="flex w-full justify-between px-6 flex-wrap whitespace-nowrap ">
-      <div className="w-[233px] h-[37] pt-4 flex md:inline-block ">
-        <img
-          src={logo}
-          alt="logo"
-          className="w-[150px] md:w-[233px] md:h-[37px]"
-        />
-      </div>
+      <Link to="/">
+        <div className="w-[233px] h-[37] pt-4 flex md:inline-block ">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[150px] md:w-[233px] md:h-[37px]"
+          />
+        </div>
+      </Link>
       <button className="md:hidden" onClick={() => setHide((prev) => !prev)}>
         <HambergerMenu size="30" color="#000000" />
       </button>
