cb369866a8fe83a65565c39824e47ade27c71cd3
DaScOrPiO <dunni82@yahoo.com>
2022-11-19 17:21:14 +0100

    conflict resolved

---
+++ src/pages/LandingPage/MinergramDemo/MinergramDemo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../../layouts/Navbar';
+import HeroDemo from '../MinergramDemo/DemoHero';
+
+const MinergramDemo = () => {
+  return (
+    <>
+        <Navbar />
+        <HeroDemo />
+    </>
+
+  );
+};
+
+export default MinergramDemo;
--- src/routes.jsx
+++ src/routes.jsx
@@ -9,6 +9,14 @@ import About from './pages/About/components/About';
 
 import DashboardLayout from './layouts/dashboard/DashboardLayout';
 import Dashboard from './pages/dashboard/Dashboard';
+import Images from './pages/dashboard/Images';
+import DocumentationHome from './pages/documentation/Home/DocumentationHome';
+import GettingStarted from './pages/documentation/GettingStartedPage/GettingStarted';
+import Installations from './pages/documentation/InstallationsPage/Installations';
+import ExamplePage from './pages/documentation/ExamplePage/ExamplePage';
+import UtilitiesPage from './pages/documentation/UtilitiesPage/UtilitiesPage';
+
+import Main from './pages/LandingPage/main';
 
 const RoutesComponents = () => {
   return (
@@ -16,13 +24,35 @@ const RoutesComponents = () => {
       <Routes>
         <Route path="" element={<Layout />}>
           <Route index path="/" element={<App />}></Route>
-          <Route path="/about-us" element={<About />} />
+          <Route
+            index
+            path="/documentation"
+            element={<DocumentationHome />}
+          ></Route>
+          <Route
+            path="/documentation/getting-started"
+            element={<GettingStarted />}
+          ></Route>
+          <Route
+            path="/documentation/installations"
+            element={<Installations />}
+          ></Route>
+          <Route
+            path="/documentation/examples"
+            element={<ExamplePage/>}
+          ></Route>
+           <Route
+            path="/documentation/utilities"
+            element={<UtilitiesPage/>}
+          ></Route>
           <Route path="/careers/application" element={<CareerApplication />} />
           <Route path="*" element={<ErrorPage />}></Route>
         </Route>
         <Route path="" element={<DashboardLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/images" element={<Images />} />
         </Route>
+		    <Route path='/landingpage' element={<Main />} />
       </Routes>
     </>
   );
