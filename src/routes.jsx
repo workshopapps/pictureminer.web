f4d240d3121d73e63744bbbcfdae431760859c01
DaScOrPiO <dunni82@yahoo.com>
2022-11-19 13:45:39 +0100

    About page added

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,28 +5,27 @@ import Layout from './layouts/Layout';
 import ErrorPage from './Error';
 import App from './App';
 import { CareerApplication } from './pages/career';
+import About from './pages/About/components/About';
 
 import DashboardLayout from './layouts/dashboard/DashboardLayout';
 import Dashboard from './pages/dashboard/Dashboard';
 
 const RoutesComponents = () => {
-
-	return (
-		<>
-			<Routes>
-				<Route path="" element={<Layout />}>
-					<Route index path="/" element={<App />}></Route>
-           <Route path="/careers/application" element={<CareerApplication />} />
-					<Route path="*" element={<ErrorPage />}></Route>
-				</Route>
-				<Route path="" element={<DashboardLayout />}>
-					<Route path="/dashboard" element={<Dashboard />} />
-				</Route>
-			</Routes>
-		</>
-	);
-
-
+  return (
+    <>
+      <Routes>
+          <Route path="/about-us" element={<About />} />
+        <Route path="" element={<Layout />}>
+          <Route index path="/" element={<App />}></Route>
+          <Route path="/careers/application" element={<CareerApplication />} />
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Route>
+        <Route path="" element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default RoutesComponents;
