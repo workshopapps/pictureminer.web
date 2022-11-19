a966576ba94f20512f1038ab737252b7f345deae
Don-Alvin <alvindon41@gmail.com>
2022-11-19 15:08:29 +0300

    fixed conflicts

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-// import { Routes, Route } from 'react-router-dom';
-// import Layout from './layouts/Layout';
-// import ErrorPage from './Error';
-// import App from './App';
-// import { CareerApplication } from './pages/career';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import ErrorPage from './Error';
+import App from './App';
+import { CareerApplication } from './pages/career';
 
-// import DashboardLayout from './layouts/dashboard/DashboardLayout';
-// import Dashboard from './pages/dashboard/Dashboard';
+import DashboardLayout from './layouts/dashboard/DashboardLayout';
+import Dashboard from './pages/dashboard/Dashboard';
 
 import LandingPage from './pages/LandingPage/LandingPage';
 
@@ -16,7 +16,8 @@ const RoutesComponents = () => {
 	return (
 		<>
 			<LandingPage />
-			{/* <Routes>
+			<Routes>
+				<Route path='/landingpage' element={<LandingPage />} />
 				<Route path="" element={<Layout />}>
 					<Route index path="/" element={<App />}></Route>
            <Route path="/careers/application" element={<CareerApplication />} />
@@ -25,7 +26,7 @@ const RoutesComponents = () => {
 				<Route path="" element={<DashboardLayout />}>
 					<Route path="/dashboard" element={<Dashboard />} />
 				</Route>
-			</Routes> */}
+			</Routes>
 		</>
 	);
 
