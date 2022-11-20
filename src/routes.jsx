ce7916aa31a54bcb39403b8d61ba10b09595f199
abdulmalikyusuf <yusuffabdulmalik@gmail.com>
2022-11-20 18:32:54 +0100

    fix: fixed gradient text style&commented out styling affecting font-size

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,7 +10,6 @@ import {
 } from './pages/career';
 
 import CustomerStories from './pages/CustomerStories';
-import { CareerApplication } from './pages/career';
 import Termsofuse from './pages/terms-of-use/Termsofuse';
 
 import DashboardLayout from './layouts/dashboard/DashboardLayout';
@@ -37,10 +36,8 @@ import MinergramDemo from './pages/LandingPage/MinergramDemo/MinergramDemo';
 import ImageDetails from './pages/dashboard/ImageDetails';
 
 import Privacypolicy from './pages/privacypolicy/privacypolicy';
-import Termsofuse from './pages/terms-of-use/Termsofuse';
 
 import ApiDocumentation from './pages/documentation/ApiDocumentation';
-import ImageDetails from './pages/dashboard/ImageDetails';
 
 import Layout from './layouts/Layout';
 import HowItWorks from './pages/howItWorks/howItWorks';
