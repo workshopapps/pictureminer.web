0029b4856b5de4e1ac7c325581f17d1a7f10d089
Mayowa Sodipo <67042714+Mayorhack@users.noreply.github.com>
2022-11-21 22:14:04 +0100

    Revert "Feat/partner page"

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ErrorPage from './Error';
-<<<<<<< HEAD
 
 import {
   CareerApplication,
@@ -11,68 +10,85 @@ import {
 } from './pages/career';
 
 import CustomerStories from './pages/CustomerStories';
-import { CareerApplication } from './pages/career';
-import About from './pages/About/components/About';
-
+// import { CareerApplication } from './pages/career';
+import Termsofuse from './pages/terms-of-use/Termsofuse';
 
 import DashboardLayout from './layouts/dashboard/DashboardLayout';
 import Dashboard from './pages/dashboard/Dashboard';
 import Images from './pages/dashboard/Images';
+
+import BlogList from './pages/blog/BlogList';
+import ExtensionBlog from './pages/blog/components/ExtensionBlog';
+
+import AccountSettings from './pages/dashboard/AccountSettings';
+import Billing from './pages/dashboard/Billing';
+
 import DocumentationHome from './pages/documentation/Home/DocumentationHome';
 import GettingStarted from './pages/documentation/GettingStartedPage/GettingStarted';
 import Installations from './pages/documentation/InstallationsPage/Installations';
-import ExamplePage from './pages/documentation/ExamplePage/ExamplePage';
 import UtilitiesPage from './pages/documentation/UtilitiesPage/UtilitiesPage';
-
-import Main from './pages/LandingPage/main';
-=======
-import CustomerStories from './pages/CustomerStories';
-<<<<<<< HEAD
-import App from './App';
->>>>>>> 6650e3b (add the customerroute)
-=======
-import { CareerApplication } from './pages/career';
-import About from './pages/About/components/About';
-
-import DashboardLayout from './layouts/dashboard/DashboardLayout';
-import Dashboard from './pages/dashboard/Dashboard';
-import DocumentationHome from './pages/documentation/Home/DocumentationHome';
-import GettingStarted from './pages/documentation/GettingStartedPage/GettingStarted';
-import Installations from './pages/documentation/InstallationsPage/Installations';
 import ExamplePage from './pages/documentation/ExamplePage/ExamplePage';
-import UtilitiesPage from './pages/documentation/UtilitiesPage/UtilitiesPage';
+import Integrations from './pages/documentation/IntegrationPage/Integrations';
+import Web from './pages/documentation/WebPage/Web';
+import WhyTozilla from './pages/whyTozilla/WhyTozilla';
 
 import LandingPage from './pages/LandingPage/LandingPage';
 import MinergramDemo from './pages/LandingPage/MinergramDemo/MinergramDemo';
->>>>>>> 8b9ec0b (made some changes to file)
+import ImageDetails from './pages/dashboard/ImageDetails';
 
+import Privacypolicy from './pages/privacypolicy/privacypolicy';
+
+import ApiDocumentation from './pages/documentation/ApiDocumentation';
+import About from './pages/About/components/About';
+
+import Faq from './pages/FAQ/faq';
+
+import Layout from './layouts/Layout';
+import HowItWorks from './pages/howItWorks/howItWorks';
+import SupportPage from './pages/support';
 const RoutesComponents = () => {
   return (
     <>
       <Routes>
         <Route path="" element={<Layout />}>
-          <Route index path="/" element={<App />}></Route>
-          <Route
-            index
-            path="/documentation"
-            element={<DocumentationHome />}
-          ></Route>
+          <Route exact path="" element={<LandingPage />} />
+          <Route path="/demo" element={<MinergramDemo />} />
+          <Route path="/About-us" element={<About />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/documentation" element={<DocumentationHome />}></Route>
           <Route
             path="/documentation/getting-started"
             element={<GettingStarted />}
           ></Route>
+          <Route path="customer-stories" element={<CustomerStories />}></Route>
+          <Route path="support" element={<SupportPage />}></Route>
+          <Route
+            path="/documentation/integrations"
+            element={<Integrations />}
+          ></Route>
+          <Route path="/documentation/web" element={<Web />}></Route>
           <Route
             path="/documentation/installations"
             element={<Installations />}
           ></Route>
+
+          <Route path="/careers" element={<CareerLanding />} />
+
+          <Route path="/blog" element={<BlogList />} />
+          <Route path="/blog-page" element={<ExtensionBlog />} />
           <Route
             path="/documentation/examples"
-            element={<ExamplePage/>}
+            element={<ExamplePage />}
           ></Route>
-           <Route
+          <Route
             path="/documentation/utilities"
-            element={<UtilitiesPage/>}
+            element={<UtilitiesPage />}
           ></Route>
+          <Route
+            path="/documentation/apis"
+            element={<ApiDocumentation />}
+          ></Route>
+
           <Route path="/careers/application" element={<CareerApplication />} />
           <Route path="/careers/more-jobs" element={<CareerMoreJobs />} />
           <Route
@@ -92,15 +108,15 @@ const RoutesComponents = () => {
 
           <Route path="why-tozilla" element={<WhyTozilla />} />
         </Route>
-<<<<<<< HEAD
 
         <Route path="" element={<DashboardLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/images" element={<Images />} />
-=======
-        <Route path="customer-stories" element={<CustomerStories />}>
-          {' '}
->>>>>>> 6650e3b (add the customerroute)
+
+          <Route path="/images/:imageId" element={<ImageDetails />} />
+
+          <Route path="/account-setup" element={<AccountSettings />} />
+          <Route path="/billing" element={<Billing />} />
         </Route>
       </Routes>
     </>
