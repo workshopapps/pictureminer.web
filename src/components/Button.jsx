2d6eeca640e42ec58d8076e642cd1d8229c3c0c4
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-24 07:50:54 +0100

    feat/FE-53: made primary the default

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default function Button({ text, type }) {
+export default function Button({ text, type = 'primary' }) {
   return (
     <button
       className={`flex flex-row justify-center items-center w-[100%] h-[64px]
          rounded-lg gap-[8px] py-[16px] px-[24px] font-sans
         font-normal text-[14px] leading-[20px] ${
           type === 'primary'
-            ? 'bg-white text-[#FF6C00] border border-[#FF6C00]'
-            : 'text-white bg-[#FF6C00]'
+            ? 'text-white bg-[#FF6C00]'
+            : 'bg-white text-[#FF6C00] border border-[#FF6C00]'
         }`}
     >
       {text}
