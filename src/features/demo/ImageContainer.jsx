10813a6d5a46d62e5878006caa982e36c6a88925
taofeek-ob <taofeek01@yahoo.com>
2022-11-25 08:42:26 +0100

    feat: Drag and drop image upload

--- src/features/demo/ImageContainer.jsx
+++ src/features/demo/ImageContainer.jsx
@@ -1,30 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { images } from '../../Constants';
 import '../../styles/layout/Ecommerce.scss';
 
 const ImageContainer = () => {
+  const [dragActive, setDragActive] = useState(false);
+
+  // handle drag events
+  const handleDrag = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === 'dragenter' || e.type === 'dragover') {
+      setDragActive(true);
+    } else if (e.type === 'dragleave') {
+      setDragActive(false);
+    }
+  };
+
+  // triggers when file is dropped
+  const handleDrop = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      // at least one file has been dropped so do something
+      // handleFiles(e.dataTransfer.files);
+    }
+  };
+
+  // triggers when file is selected with click
+  const handleChange = function (e) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      // at least one file has been selected so do something
+      // handleFiles(e.target.files);
+    }
+  };
   return (
-    <div className="image_container">
-            <p className="documentation_text">
-            Try Demo
-            </p>
-            <img src={images.Vector} alt="Vector" />
-            <p>Drag and drop image here or click to upload</p>
-            <div className="block flex my-4">
-            <button
-              className="btn w-1/2 px-4 text-sm text-white-600 border border-slate-400 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
-              type="submit"
-            >
-              Upload image
-            </button>
-            <input
-              className="input placeholder:normal placeholder:text-slate-400 block bg-white w-full border border-slate-400 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Place URL"
-              type="text"
-              name="search"
-            />
-          </div>
-          </div>
+    <form
+      className="image_container"
+      onDragEnter={handleDrag}
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <p className="documentation_text">Try Demo</p>
+      <img src={images.Vector} alt="Vector" />
+      <label
+        id="label-file-upload"
+        htmlFor="input-file-upload"
+        className={dragActive ? 'drag-active' : ''}
+      >
+        <div className="block flex my-4">
+          <input
+            className="input placeholder:normal placeholder:text-slate-400 block bg-white w-full border border-slate-400 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="Place URL"
+            type="file"
+            name="search"
+            onChange={handleChange}
+          />
+        </div>
+      </label>
+      {dragActive && (
+        <div
+          id="drag-file-element"
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+        ></div>
+      )}
+    </form>
   );
 };
 
