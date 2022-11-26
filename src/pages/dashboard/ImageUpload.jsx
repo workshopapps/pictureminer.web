e9bd6ee2bd832495fe373cdf5564794033272051
taofeek-ob <taofeek01@yahoo.com>
2022-11-26 08:34:50 +0100

    fixing netlify build errors

--- src/pages/dashboard/ImageUpload.jsx
+++ src/pages/dashboard/ImageUpload.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { DocumentUpload } from 'iconsax-react';
 import { images } from '../../Constants';
-import useUploadImage from '../../hooks/useUploadImage';
+import useUploadImage from '../../Hooks/useUploadImage';
 import './styles/imageUpload.scss';
 
 const ImageUpload = () => {
