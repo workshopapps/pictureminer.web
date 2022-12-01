f1feda2b99490b0b6354459870d1324a269593d8
TTUmejiaku <templeumejiaku@gmail.com>
2022-12-01 19:28:38 +0100

    chore: Changed upload csv file to upload image

--- src/pages/LandingPage/SearchEnginePage/SearchEngine.jsx
+++ src/pages/LandingPage/SearchEnginePage/SearchEngine.jsx
@@ -13,7 +13,7 @@ const SearchEngine = () => {
         <div className="container__search-engine flex flex-col md:flex-row items-center justify-between">
           <div className="text-white md:w-[45%] lg:w-[43%] text-center md:text-left shrink-0">
             <h1 className="text-large font-bold">Search Engine</h1>
-            <p className="mt-6 font-light">
+            <p className="mt-6 tracking-wide">
               Using artificial intelligence , Discripto technology enables
               automatic assignment of images to relevant categories . This
               automated image categorization technology saves time and budget
@@ -28,11 +28,11 @@ const SearchEngine = () => {
       </section>
       <section>
         <div className="container__search-engine my-16 space-y-20">
-          {/* second layout */}
+          {/* first layout */}
           <div className="flex flex-col md:flex-row-reverse items-center  md:gap-12 lg:mr-52 lg:gap-24">
             <div className="text-center md:text-left">
               <h3 className="text-normal font-bold">Structures Descriptions</h3>
-              <p className="font-light mt-4">
+              <p className=" mt-4">
                 Our API creates structured descriptions for image databases, for
                 huge online picture libraries, saving them time, stress, and
                 manual labour.
@@ -48,7 +48,7 @@ const SearchEngine = () => {
               <h3 className="text-normal font-bold">
                 Precise Image Description
               </h3>
-              <p className="font-light mt-4">
+              <p className=" mt-4">
                 Our API analyzes your images to collect details like color,
                 texture etc, and then creates a query to match with other images
                 for the best and most similar matches.
@@ -64,7 +64,7 @@ const SearchEngine = () => {
               <h3 className="text-normal font-bold">
                 Useful For Business Analysis
               </h3>
-              <p className="font-light mt-4">
+              <p className=" mt-4">
                 Oue API also makes it possible to access and evaluate the
                 images’ pattern and search result distributions, which is useful
                 for SEO projects and business assessment analysis.
