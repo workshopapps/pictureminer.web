d5458b56c68cf88f73fa36abc1d34d28b4129a17
yewande-hassan <yewandehassan2@gmail.com>
2022-12-01 22:42:20 +0100

    Implementation of the Why Discripto page

--- src/pages/whyTozilla/WhyMinergram.jsx
+++ src/pages/whyTozilla/WhyMinergram.jsx
@@ -1,52 +1,37 @@
 import React from 'react';
 import { features } from './data/features';
 import Computer from './assets/computer.webp';
-import { testimonials } from './data/testimonials';
-import Testimonial from './components/testimonial';
+
 import Feature from './components/feature';
 
 function WhyMinergram() {
   return (
     <main className="font-Axiforma">
-      <section className="flex flex-col md:flex-row items-center gap-y-[18px] px-[22px] md:px-20">
-        <div className="md:max-w-[399px] lg:max-w-[487px]">
-          <h4 className="font-medium text-2xl lg:text-[35px] lg:leading-[56px] font-Axiforma text-[#061D33]">
-            Why Should you Choose to use Discripto API?
+      <section className="flex flex-col md:flex-col max-[480px]:max-w-full max-[480px]:p-[25px] items-center m-auto max-w-[60%] my-[30px] min-[1024px]:pt-[25px]">
+        <div className="text-center">
+          <h4 className="font-medium text-2xl lg:leading-[56px] font-Axiforma text-[#061D33] max-[480px]:text-2xl min-[1024px]:text-4xl min-[1200px]:text-5xl">
+            Why Choose Discripto API?
           </h4>
           <p className="mt-[18px] md:mt-5 lg:mt-[25px] text-xs leading-[18px] lg:text-lg lg:leading-7 text-[#08253F]">
-            Discripto is a game-changing and very accurate image recognition API
-            that makes life easier by digitizing the many laborious data
-            gathering, processing, and increasing production processes while
-            also improving the quality of our daily lives. Discripto helps you
-            gain valuable insight into your operations that enables true
-            business transformation and is widely used in industries.
+          Discripto is an accurate image recognition API that makes life easier by digitizing the many laborious data gathering, processing, and increasing production processes.
           </p>
+          <button className="items-center w-[200px] h-[48px] bg-[#FF6C00] rounded-lg gap-[8px] py-[16px] px-[24px] font-sans font-normal text-[14px] leading-[20px] text-white hover:bg-[#FF9D55] my-[50px] ">
+          Get Started
+          </button>
         </div>
         <div>
           <img
             src={Computer}
             alt="Image of computer and a woman"
-            className="object-cover"
+            className="object-cover max-[480px]:hidden"
           />
         </div>
       </section>
-      <section className="md:gap-x-10 gap-y-5 flex flex-wrap justify-center lg:justify-start mx-[114px] md:mx-[196px] lg:mx-[130px] mt-[18px] md:mt-[58px] mb-11 md:mb-[74px] lg:my-[120px]">
-        {features.map((feature) => (
-          <Feature key={feature.title} {...feature} />
-        ))}
-      </section>
-      <section className="py-[15px] md:py-[45px] lg:py-20 px-[27px] md:px-20 lg:px-[158px] bg-[#E0E0E0]">
-        <div className="flex items-center justify-between w-[366px] text-black">
-          <h4 className="font-medium text-sm md:text-3xl md:leading-[38px] lg:text-4xl lg:leading-[44px] text-[#051626] -tracking-[0.02em] font-Axiforma">
-            Testimonials
-          </h4>
-          <p className="text-xs leading-[18px] font-Axiforma md:hidden">
-            see more
-          </p>
-        </div>
-        <div className="flex gap-x-[30px] md:gap-x-5 lg:gap-x-[21px] md:mt-[15px] lg:mt-10">
-          {testimonials.map((testimonial, idx) => (
-            <Testimonial key={idx} {...testimonial} />
+      <section className="bg-[#F7F7F7] lg:py-[100px] lg:px-[50px] md:mt-[58px] lg:mt-[120px]">
+        <p className='text-center text-4xl font-medium py-[20px] max-[480px]:text-2xl'>Our Unique Selling Point</p>
+        <div className='grid md:grid-cols-2 md:gap-4 lg:grid-cols-4 lg:gap-8 lg:px-[50px] md:px-[100px] md:py-[30px] min-[1024px]:gap-5'>
+          {features.map((feature) => (
+            <Feature key={feature.title} {...feature}/>
           ))}
         </div>
       </section>
