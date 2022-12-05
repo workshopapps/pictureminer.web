6a4d9f4ccd5b3a8dd03745c1b7c2cb7f984eadd9
TTUmejiaku <templeumejiaku@gmail.com>
2022-12-05 08:30:05 +0100

    fix: Uncomment eslint

--- src/pages/FAQ/faq.jsx
+++ src/pages/FAQ/faq.jsx
@@ -5,6 +5,7 @@ import {
   MdOutlineKeyboardArrowDown,
   MdOutlineKeyboardArrowUp,
 } from 'react-icons/md';
+// import { Link } from 'react-router-dom';
 
 const Faq = () => {
   const [clicked, setClicked] = useState(false);
@@ -24,6 +25,7 @@ const Faq = () => {
           className={`w-11/12 md:w-4/5 mx-auto p-4 border-b border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100/50 ${
             clicked === index ? 'bg-gray-100/50' : ''
           } `}
+          key={faq.id + 1}
         >
           <div
             className="flex items-center justify-between text-gray-500 cursor-pointer"
@@ -41,6 +43,10 @@ const Faq = () => {
               {faq.answer.map((answer) => (
                 <p className="my-3 text-gray-500 " key={answer}>
                   {answer}
+                  {/* {answer.replace(
+                    '<Link>',
+                    <Link to="/pricing">Click me</Link>
+                  )} */}
                 </p>
               ))}
             </div>
--- src/pages/FAQ/faqData.js
+++ src/pages/FAQ/faqData.js
@@ -1,5 +1,3 @@
-// import { Link } from 'react-router-dom';
-
 export const faqData = [
   {
     id: 0,
