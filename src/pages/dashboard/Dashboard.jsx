ec313701a0d990b3f961f2ab5a57e9ecd78bd16d
Ademola Ogunmokun <ademolaogunmokun@yahoo.com>
2022-12-10 08:41:44 +0000

    populate chart with dummy data

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import UserContext from '../../context/UserContext';
 import useGetBatch from '../../Hooks/useGetBatch';
-import { ResponsiveContainer, Legend, BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip } from 'recharts';
+import { ResponsiveContainer, Legend, BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip, LabelList} from 'recharts';
 import { notifyError } from '../../utils/notify';
 
 axios.defaults.baseURL = 'https://discripto.hng.tech/api1/api/v1/';
@@ -13,12 +13,49 @@ const Dashboard = () => {
   const { user } = useContext(UserContext);
   const { response:batchImages } = useGetBatch();
   
-  const BarData = batchImages.map((batch) => {
-    return {
-      name: batch.batchName,
-      images: batch.images.length,
-    }
-  })
+  const BarData = [
+    {
+      "id": "638b70f437a213abb85cb30a",
+      "tagged" : 1,
+       "untagged" : 3,
+       "total" : 4,
+      "date_created": new Date("2022-12-03T15:53:24.196Z").toLocaleDateString('en-US',
+      { month: 'short', day: 'numeric', year: 'numeric' }),
+    },
+    {
+      "id": "638b70f437a213abb85cb30a",
+      "tagged" : 1,
+       "untagged" : 3,
+       "total" : 4,
+      "date_created": new Date("2022-12-03T15:53:24.196Z").toLocaleDateString('en-US',
+      { month: 'short', day: 'numeric', year: 'numeric' }),
+    },
+    {
+      "id": "638b70f437a213abb85cb30a",
+      "tagged" : 1,
+       "untagged" : 3,
+       "total" : 4,
+      "date_created": new Date("2022-12-03T15:53:24.196Z").toLocaleDateString('en-US',
+      { month: 'short', day: 'numeric', year: 'numeric' }),
+    },
+    {
+      "id": "638b70f437a213abb85cb30a",
+      "tagged" : 1,
+       "untagged" : 3,
+       "total" : 4,
+      "date_created": new Date("2022-12-04T15:53:24.196Z").toLocaleDateString('en-US',
+      { month: 'short', day: 'numeric', year: 'numeric' }),
+    },
+    {
+      "id": "638b70f437a213abb85cb30a",
+      "tagged" : 1,
+       "untagged" : 3,
+       "total" : 4,
+      "date_created": new Date("2022-12-05T15:53:24.196Z").toLocaleDateString('en-US',
+      { month: 'short', day: 'numeric', year: 'numeric' }),
+    },
+    
+     ]
   
   
 
@@ -141,22 +178,43 @@ const Dashboard = () => {
         <BarChart
           width={500}
           height={300}
-          data={barData}
+          data={BarData}
           margin={{
             top: 5,
-            right: 5,
-            left: 5,
+            right: 10,
+            left: 10,
             bottom: 5,
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <XAxis dataKey = 'date_created'
+          label={{ value: 'Date Mined', position: 'bottom', offset: 0, fontSize: '16px',
+          fontWeight: 'bold',
+          position: 'insideBottomRight', dy: 10,
+          fill: '#000000'
+
+        }}
+          
+          />
+          <YAxis 
+          label={{ value: 'Total', 
+          fontWeight: 'bold',
+          position: 'insideLeft',
+          fill: '#000000',
+          angle: -90
+      }}
+          
+          />
           <Tooltip />
           <Legend />
-          <Bar dataKey="Total Batch Upload" fill="#8884d8" />
-          <Bar dataKey="Tagged" fill="#82ca9d" />
-          <Bar dataKey="Untagged" fill="#FF8042" />
+          <Bar dataKey="tagged" stackId="a" fill="#8884d8"  >
+          <LabelList dataKey="tagged" position="top" />
+          </Bar>
+          <Bar dataKey="untagged" stackId="a" fill="#82ca9d" >
+          <LabelList dataKey="untagged" position="top" />
+          </Bar>
+         
+
         </BarChart>
         </ResponsiveContainer>
 
