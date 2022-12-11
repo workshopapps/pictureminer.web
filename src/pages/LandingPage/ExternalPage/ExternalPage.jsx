58565e1f5dde3df5ecf8492a0202ae93a491e8c5
Mayowa Sodipo <67042714+Mayorhack@users.noreply.github.com>
2022-12-11 21:52:34 +0100

    fix:link

--- src/pages/LandingPage/ExternalPage/ExternalPage.jsx
+++ src/pages/LandingPage/ExternalPage/ExternalPage.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-trailing-spaces */
-import React, { useState } from 'react';
-
-import ImageUpload from '../../dashboard/ImageUpload';
-import '../../../styles/layout/Ecommerce.scss';
-import YouTube from 'react-youtube';
-import Button from '../../../components/Button';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ImageUpload from "../../dashboard/ImageUpload";
+import "../../../styles/layout/Ecommerce.scss";
+import YouTube from "react-youtube";
+import Button from "../../../components/Button";
 
 const opts = {
-  height: '390',
-  width: '640',
+  height: "390",
+  width: "640",
   playerVars: {
     // https://developers.google.com/youtube/player_parameters
     autoplay: 1,
@@ -36,26 +36,28 @@ const ExternalPage = () => {
               }
             />
             {showContainer.button ? (
-              <Button
-                text={'Upload Image'}
-                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-[#FF6C00] hover:bg-[#FF9D55] disabled:bg-[#FF9D55] disabled:cursor-not-allowed flex flex-row justify-center items-center w-[200] 
+              <Link to="/signup">
+                <Button
+                  text={"Upload Image"}
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-[#FF6C00] hover:bg-[#FF9D55] disabled:bg-[#FF9D55] disabled:cursor-not-allowed flex flex-row justify-center items-center w-[200] 
             rounded-lg gap-[8px] py-[16px] px-[24px] font-sans
            font-normal text-[14px] leading-[20px]"
-                onClick={() => {
-                  setShowContainer((prev) => {
-                    return {
-                      ...prev,
-                      container: true,
-                    };
-                  });
-                }}
-              />
+                  onClick={() => {
+                    setShowContainer((prev) => {
+                      return {
+                        ...prev,
+                        container: true,
+                      };
+                    });
+                  }}
+                />
+              </Link>
             ) : null}
           </div>
         ) : (
           <>
             <Button
-              text={'Batch Upload >>>>>'}
+              text={"Batch Upload >>>>>"}
               className=" text-white bg-[#FF6C00] hover:bg-[#FF9D55] disabled:bg-[#FF9D55] disabled:cursor-not-allowed flex flex-row justify-end items-center w-[200] 
             rounded-lg gap-[8px] py-[16px] px-[24px] font-sans
            font-normal text-[14px] leading-[20px] ml-auto mt-7 mr-5"
