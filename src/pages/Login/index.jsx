fd67d4b86420a57fb4b53b9dfb3127b09e47ab12
Ademola Ogunmokun <ademolaogunmokun@yahoo.com>
2022-12-13 17:45:40 +0000

    chnage login animation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -39,10 +39,10 @@ const index = () => {
   };
   return (
     <motion.div
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-
+    initial={{ opacity: 0, x: -300 }}
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, x: 300 }}
+    transition={{ type: 'spring', duration: 1, ease: 'easeOut' }}
 
     className="logincontainer ">
       <LoginHeader />
